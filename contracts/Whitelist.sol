c24aaceace34af4738db244c49777c690c1a1388
alpha@DESKTOP-6F6AEHR <blkcdev@gmail.com>
2022-09-11 11:17:38 -0500

    added variables to control max no of address to be added to tree

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -7,8 +7,15 @@ contract Whitelist {
 
     bytes32 public merkleRoot;
 
-    constructor(bytes32 _merkleRoot) {
+    // Max number of whitelisted addresses allowed to be added to tree
+    uint8 public maxWhitelistedAddresses;
+    //space used so far. 
+    //(space counter which tracks spaces already used out of maxWhitelistedAddresses)
+    uint8 public numAddressesWhitelisted;
+
+    constructor(bytes32 _merkleRoot,uint8 _maxWhitelistedAddresses) {
         merkleRoot = _merkleRoot;
+        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
     }
 
     function checkInWhitelist(bytes32[] calldata proof, uint64 maxAllowanceToMint) view public returns (bool) {
@@ -16,5 +23,7 @@ contract Whitelist {
         bool verified = MerkleProof.verify(proof, merkleRoot, leaf);
         return verified;
     }
+
+
     
 }
